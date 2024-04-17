776fa1b73f58df1252190df1cbd747129183eae1
Äkwav <ekwav@coflnet.com>
2024-04-17 11:52:31 +0200

    Validate cassandra keyspace and log errors

--- Cassandra/CassandraServiceExtensions.cs
+++ Cassandra/CassandraServiceExtensions.cs
@@ -1,5 +1,6 @@
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using Cassandra;
 using Coflnet.Core;
 using Microsoft.Extensions.Configuration;
@@ -26,13 +27,18 @@ public static class CassandraServiceExtensions
         var Configuration = p.GetRequiredService<IConfiguration>();
         var section = Configuration.GetSection(sectionName);
         Console.WriteLine($"Connecting to {sectionName}...");
+        var keyspace = section["KEYSPACE"];
         var builder = Cluster.Builder().AddContactPoints(section["HOSTS"]?.Split(","))
             .WithLoadBalancingPolicy(new TokenAwarePolicy(new DCAwareRoundRobinPolicy()))
             .WithCredentials(section["USER"], section["PASSWORD"])
-            .WithDefaultKeyspace(section["KEYSPACE"]);
-
+            .WithDefaultKeyspace(keyspace);
+        
+        if (keyspace == null)
+            throw new InvalidOperationException($"{sectionName}:KEYSPACE must be set to a keyspace name.");
+        if (Regex.IsMatch(keyspace, @"[^a-zA-Z0-9_]"))
+            throw new InvalidOperationException($"{sectionName}:KEYSPACE must only contain alphanumeric characters and underscores. See https://cassandra.apache.org/doc/stable/cassandra/cql/ddl.html#common-definitions");
         Console.WriteLine("Connecting to servers " + section["HOSTS"]);
-        Console.WriteLine("Using keyspace " + section["KEYSPACE"]);
+        Console.WriteLine("Using keyspace " + keyspace);
         Console.WriteLine("Using replication class " + section["REPLICATION_CLASS"]);
         Console.WriteLine("Using replication factor " + section["REPLICATION_FACTOR"]);
         Console.WriteLine("Using user " + section["USER"]);
@@ -44,7 +50,7 @@ public static class CassandraServiceExtensions
         if (!string.IsNullOrEmpty(certificatePaths))
         {
             var password = section["X509Certificate_PASSWORD"]
-                ?? throw new InvalidOperationException("CASSANDRA:X509Certificate_PASSWORD must be set if CASSANDRA:X509Certificate_PATHS is set.");
+                ?? throw new InvalidOperationException($"{sectionName}:X509Certificate_PASSWORD must be set if {sectionName}:X509Certificate_PATHS is set.");
             CustomRootCaCertificateValidator? certificateValidator = null;
             if (!string.IsNullOrEmpty(validationCertificatePath))
                 certificateValidator = new CustomRootCaCertificateValidator(new X509Certificate2(validationCertificatePath, password));
@@ -59,6 +65,7 @@ public static class CassandraServiceExtensions
             builder.WithSSL(sslOptions);
         }
         var cluster = builder.Build();
+        // connect without default key namespace
         var session = cluster.Connect(null);
         var defaultKeyspace = cluster.Configuration.ClientOptions.DefaultKeyspace;
         try
@@ -74,6 +81,10 @@ public static class CassandraServiceExtensions
         catch (UnauthorizedException)
         {
             Console.WriteLine("User unauthorized to create keyspace, trying to switch it");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error creating keyspace: " + e);
         }
         finally
         {
